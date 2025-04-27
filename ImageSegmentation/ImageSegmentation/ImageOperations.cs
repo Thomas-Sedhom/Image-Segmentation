6a526de204329c1a42be4e756b419b4cd44ad893
thomas sedhom <thomassedhom97@gmail.com>
2025-04-28 01:18:56 +0300

    Initialize

---
+++ ImageSegmentation/ImageSegmentation/ImageOperations.cs
@@ -0,0 +1,311 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+///Algorithms Project
+///Intelligent Scissors
+///
+
+namespace ImageTemplate
+{
+    /// <summary>
+    /// Holds the pixel color in 3 byte values: red, green and blue
+    /// </summary>
+    public struct RGBPixel
+    {
+        public byte red, green, blue;
+    }
+    public struct RGBPixelD
+    {
+        public double red, green, blue;
+    }
+    
+    public struct Node
+    {
+        public int x, y;
+        public List<KeyValuePair<Node, int>> children;
+
+        public Node(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            children = new List<KeyValuePair<Node, int>>();
+        }
+    }
+
+    
+  
+    /// <summary>
+    /// Library of static functions that deal with images
+    /// </summary>
+    public class ImageOperations
+    {
+        /// <summary>
+        /// Open an image and load it into 2D array of colors (size: Height x Width)
+        /// </summary>
+        /// <param name="ImagePath">Image file path</param>
+        /// <returns>2D array of colors</returns>
+        public static RGBPixel[,] OpenImage(string ImagePath)
+        {
+            Bitmap original_bm = new Bitmap(ImagePath);
+            int Height = original_bm.Height;
+            int Width = original_bm.Width;
+
+            RGBPixel[,] Buffer = new RGBPixel[Height, Width];
+
+            unsafe
+            {
+                BitmapData bmd = original_bm.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadWrite, original_bm.PixelFormat);
+                int x, y;
+                int nWidth = 0;
+                bool Format32 = false;
+                bool Format24 = false;
+                bool Format8 = false;
+
+                if (original_bm.PixelFormat == PixelFormat.Format24bppRgb)
+                {
+                    Format24 = true;
+                    nWidth = Width * 3;
+                }
+                else if (original_bm.PixelFormat == PixelFormat.Format32bppArgb || original_bm.PixelFormat == PixelFormat.Format32bppRgb || original_bm.PixelFormat == PixelFormat.Format32bppPArgb)
+                {
+                    Format32 = true;
+                    nWidth = Width * 4;
+                }
+                else if (original_bm.PixelFormat == PixelFormat.Format8bppIndexed)
+                {
+                    Format8 = true;
+                    nWidth = Width;
+                }
+                int nOffset = bmd.Stride - nWidth;
+                byte* p = (byte*)bmd.Scan0;
+                for (y = 0; y < Height; y++)
+                {
+                    for (x = 0; x < Width; x++)
+                    {
+                        if (Format8)
+                        {
+                            Buffer[y, x].red = Buffer[y, x].green = Buffer[y, x].blue = p[0];
+                            p++;
+                        }
+                        else
+                        {
+                            Buffer[y, x].red = p[2];
+                            Buffer[y, x].green = p[1];
+                            Buffer[y, x].blue = p[0];
+                            if (Format24) p += 3;
+                            else if (Format32) p += 4;
+                        }
+                    }
+                    p += nOffset;
+                }
+                original_bm.UnlockBits(bmd);
+            }
+
+            return Buffer;
+        }
+        
+        /// <summary>
+        /// Get the height of the image 
+        /// </summary>
+        /// <param name="ImageMatrix">2D array that contains the image</param>
+        /// <returns>Image Height</returns>
+        public static int GetHeight(RGBPixel[,] ImageMatrix)
+        {
+            return ImageMatrix.GetLength(0);
+        }
+
+        /// <summary>
+        /// Get the width of the image 
+        /// </summary>
+        /// <param name="ImageMatrix">2D array that contains the image</param>
+        /// <returns>Image Width</returns>
+        public static int GetWidth(RGBPixel[,] ImageMatrix)
+        {
+            return ImageMatrix.GetLength(1);
+        }
+
+        /// <summary>
+        /// Display the given image on the given PictureBox object
+        /// </summary>
+        /// <param name="ImageMatrix">2D array that contains the image</param>
+        /// <param name="PicBox">PictureBox object to display the image on it</param>
+        public static void DisplayImage(RGBPixel[,] ImageMatrix, PictureBox PicBox)
+        {
+            // Create Image:
+            //==============
+            int Height = ImageMatrix.GetLength(0);
+            int Width = ImageMatrix.GetLength(1);
+
+            Bitmap ImageBMP = new Bitmap(Width, Height, PixelFormat.Format24bppRgb);
+
+            unsafe
+            {
+                BitmapData bmd = ImageBMP.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadWrite, ImageBMP.PixelFormat);
+                int nWidth = 0;
+                nWidth = Width * 3;
+                int nOffset = bmd.Stride - nWidth;
+                byte* p = (byte*)bmd.Scan0;
+                for (int i = 0; i < Height; i++)
+                {
+                    for (int j = 0; j < Width; j++)
+                    {
+                        p[2] = ImageMatrix[i, j].red;
+                        p[1] = ImageMatrix[i, j].green;
+                        p[0] = ImageMatrix[i, j].blue;
+                        p += 3;
+                    }
+
+                    p += nOffset;
+                }
+                ImageBMP.UnlockBits(bmd);
+            }
+            PicBox.Image = ImageBMP;
+        }
+
+
+       /// <summary>
+       /// Apply Gaussian smoothing filter to enhance the edge detection 
+       /// </summary>
+       /// <param name="ImageMatrix">Colored image matrix</param>
+       /// <param name="filterSize">Gaussian mask size</param>
+       /// <param name="sigma">Gaussian sigma</param>
+       /// <returns>smoothed color image</returns>
+        public static RGBPixel[,] GaussianFilter1D(RGBPixel[,] ImageMatrix, int filterSize, double sigma)
+        {
+            int Height = GetHeight(ImageMatrix);
+            int Width = GetWidth(ImageMatrix);
+
+            RGBPixelD[,] VerFiltered = new RGBPixelD[Height, Width];
+            RGBPixel[,] Filtered = new RGBPixel[Height, Width];
+
+           
+            // Create Filter in Spatial Domain:
+            //=================================
+            //make the filter ODD size
+            if (filterSize % 2 == 0) filterSize++;
+
+            double[] Filter = new double[filterSize];
+
+            //Compute Filter in Spatial Domain :
+            //==================================
+            double Sum1 = 0;
+            int HalfSize = filterSize / 2;
+            for (int y = -HalfSize; y <= HalfSize; y++)
+            {
+                //Filter[y+HalfSize] = (1.0 / (Math.Sqrt(2 * 22.0/7.0) * Segma)) * Math.Exp(-(double)(y*y) / (double)(2 * Segma * Segma)) ;
+                Filter[y + HalfSize] = Math.Exp(-(double)(y * y) / (double)(2 * sigma * sigma));
+                Sum1 += Filter[y + HalfSize];
+            }
+            for (int y = -HalfSize; y <= HalfSize; y++)
+            {
+                Filter[y + HalfSize] /= Sum1;
+            }
+
+            //Filter Original Image Vertically:
+            //=================================
+            int ii, jj;
+            RGBPixelD Sum;
+            RGBPixel Item1;
+            RGBPixelD Item2;
+
+            for (int j = 0; j < Width; j++)
+                for (int i = 0; i < Height; i++)
+                {
+                    Sum.red = 0;
+                    Sum.green = 0;
+                    Sum.blue = 0;
+                    for (int y = -HalfSize; y <= HalfSize; y++)
+                    {
+                        ii = i + y;
+                        if (ii >= 0 && ii < Height)
+                        {
+                            Item1 = ImageMatrix[ii, j];
+                            Sum.red += Filter[y + HalfSize] * Item1.red;
+                            Sum.green += Filter[y + HalfSize] * Item1.green;
+                            Sum.blue += Filter[y + HalfSize] * Item1.blue;
+                        }
+                    }
+                    VerFiltered[i, j] = Sum;
+                }
+
+            //Filter Resulting Image Horizontally:
+            //===================================
+            for (int i = 0; i < Height; i++)
+                for (int j = 0; j < Width; j++)
+                {
+                    Sum.red = 0;
+                    Sum.green = 0;
+                    Sum.blue = 0;
+                    for (int x = -HalfSize; x <= HalfSize; x++)
+                    {
+                        jj = j + x;
+                        if (jj >= 0 && jj < Width)
+                        {
+                            Item2 = VerFiltered[i, jj];
+                            Sum.red += Filter[x + HalfSize] * Item2.red;
+                            Sum.green += Filter[x + HalfSize] * Item2.green;
+                            Sum.blue += Filter[x + HalfSize] * Item2.blue;
+                        }
+                    }
+                    Filtered[i, j].red = (byte)Sum.red;
+                    Filtered[i, j].green = (byte)Sum.green;
+                    Filtered[i, j].blue = (byte)Sum.blue;
+                }
+
+            return Filtered;
+        }
+       
+       
+        public static Node[,] GraphConstruct(RGBPixel[,] ImageMatrix, string color)
+        {
+            RGBPixel[,] filteredImageMatrix = GaussianFilter1D(ImageMatrix, 4, 0.8);
+            Node[,] graph = new Node[ImageMatrix.GetLength(0), ImageMatrix.GetLength(1)];
+
+            for (int i = 0; i < filteredImageMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < filteredImageMatrix.GetLength(1); j++)
+                {
+                    graph[i, j] = new Node(i, j);
+                }
+            }
+
+            int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
+            int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+            for (int i = 0; i < ImageMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < ImageMatrix.GetLength(1); j++)
+                {
+                    for (int k = 0; k < 8; k++) 
+                    {
+                        int x = i + dx[k];
+                        int y = j + dy[k];
+
+                        if (x >= 0 && x < ImageMatrix.GetLength(0) && y >= 0 && y < ImageMatrix.GetLength(1))
+                        {
+                            int weight;
+                            if (color == "red")
+                                weight = Math.Abs(filteredImageMatrix[i, j].red - filteredImageMatrix[x, y].red);
+                            else if (color == "green")
+                                weight = Math.Abs(filteredImageMatrix[i, j].green - filteredImageMatrix[x, y].green);
+                            else
+                                weight =Math.Abs(filteredImageMatrix[i, j].blue - filteredImageMatrix[x, y].blue);
+                            graph[i, j].children.Add(new KeyValuePair<Node, int>(graph[x, y], weight));
+                        }
+                    }
+                }
+            }
+
+            return graph;
+        }
+        public static RGBPixel[,] ImageSegmentation(RGBPixel[,] ImageMatrix, string color)
+        {
+            List<KeyValuePair<int, KeyValuePair<Node, Node>>> nodes = new List<KeyValuePair<int, KeyValuePair<Node, Node>>>();
+            return ImageMatrix;
+        }
+    }
+
+}
