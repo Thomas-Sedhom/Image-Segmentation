c221ce8a5decab03bb4ba2aad9b5ba388ef513be
galal <galalmohamed25325@gmail.com>
2025-05-12 18:04:12 +0300

    edges

--- ImageSegmentation/ImageSegmentation/Segmentation.cs
+++ ImageSegmentation/ImageSegmentation/Segmentation.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
 
 namespace ImageTemplate
 {
@@ -52,82 +53,104 @@ namespace ImageTemplate
             children = new List<KeyValuePair<Node, int>>();
         }
     }
+    public class Edge
+    {
+        public (int x, int y) From;
+        public (int x, int y) To;
+        public int Weight;
+
+        public Edge((int x, int y) from, (int x, int y) to, int weight)
+        {
+            From = from;
+            To = to;
+            Weight = weight;
+        }
+    }
 
     internal class Segmentation
     {
 
+
         // Exact(N^2)
-        private static Node[,] GraphConstruct(RGBPixel[,] ImageMatrix, string color)
+        private static (Dictionary<(int, int), List<Edge>> adjacencyList, List<Edge> sortedEdges) GraphConstruct(RGBPixel[,] ImageMatrix, string color)
         {
-            Node[,] graph = new Node[ImageMatrix.GetLength(0), ImageMatrix.GetLength(1)];
-
-            int uuid = 0;
-            for (int i = 0; i < ImageMatrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < ImageMatrix.GetLength(1); j++)
-                {
-                    graph[i, j] = new Node(uuid++, i, j);
-                }
-            }
-
             int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
             int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
-            for (int i = 0; i < ImageMatrix.GetLength(0); i++)
+            int height = ImageMatrix.GetLength(0);
+            int width = ImageMatrix.GetLength(1);
+
+            var graph = new Dictionary<(int, int), List<Edge>>();
+            var edgeSet = new HashSet<(int, int, int, int)>();  // To prevent duplicates
+            var allEdges = new List<Edge>();
+
+            for (int i = 0; i < height; i++)
             {
-                for (int j = 0; j < ImageMatrix.GetLength(1); j++)
+                for (int j = 0; j < width; j++)
                 {
+                    var currentKey = (i, j);
+                    if (!graph.ContainsKey(currentKey))
+                        graph[currentKey] = new List<Edge>();
+
                     for (int k = 0; k < 8; k++)
                     {
                         int x = i + dx[k];
                         int y = j + dy[k];
+                        var neighborKey = (x, y);
 
-                        if (x >= 0 && x < ImageMatrix.GetLength(0) && y >= 0 && y < ImageMatrix.GetLength(1))
+                        if (x >= 0 && x < height && y >= 0 && y < width)
                         {
+                            // Avoid duplicate edges by enforcing (min, max) order
+                            var edgeId = (Math.Min(i, x), Math.Min(j, y), Math.Max(i, x), Math.Max(j, y));
+                            if (edgeSet.Contains(edgeId))
+                                continue;
+
+                            edgeSet.Add(edgeId);
+
                             int weight;
                             if (color == "red")
-                                weight = Math.Abs(ImageMatrix[i, j].red - ImageMatrix[x, y].red);
+                                weight = Math.Abs((int)ImageMatrix[i, j].red - (int)ImageMatrix[x, y].red);
                             else if (color == "green")
-                                weight = Math.Abs(ImageMatrix[i, j].green - ImageMatrix[x, y].green);
+                                weight = Math.Abs((int)ImageMatrix[i, j].green - (int)ImageMatrix[x, y].green);
                             else
-                                weight = Math.Abs(ImageMatrix[i, j].blue - ImageMatrix[x, y].blue);
-                            //if (weight > 0)
-                            //    Console.WriteLine(" x: " + x + " y: " + y + "  i: " + i + " j: " + j + " weight " + weight);
-                            graph[i, j].children.Add(new KeyValuePair<Node, int>(graph[x, y], weight));
+                                weight = Math.Abs((int)ImageMatrix[i, j].blue - (int)ImageMatrix[x, y].blue);
+
+                            // Create undirected edge (stored once)
+                            var edge = new Edge(currentKey, neighborKey, weight);
+                            allEdges.Add(edge);
+
+                            // Add to adjacency list both ways
+                            graph[currentKey].Add(new Edge(currentKey, neighborKey, weight));
+                            if (!graph.ContainsKey(neighborKey))
+                                graph[neighborKey] = new List<Edge>();
+                            graph[neighborKey].Add(new Edge(neighborKey, currentKey, weight));
                         }
                     }
                 }
             }
-            return graph;
+
+            // Sort global edge list by weight
+            allEdges.Sort((a, b) => a.Weight.CompareTo(b.Weight));
+
+            return (graph, allEdges);
         }
 
-        // Exact(M)
-        private static DisjointSet ImageSegmentation(Node[,] graph)
-        {
-            List<KeyValuePair<int, KeyValuePair<Node, Node>>> nodes = new List<KeyValuePair<int, KeyValuePair<Node, Node>>>();
-            for (int i = 0; i < graph.GetLength(0); i++)
-            {
-                for (int j = 0; j < graph.GetLength(1); j++)
-                {
-                    foreach (KeyValuePair<Node, int> child in graph[i, j].children)
-                    {
-                        nodes.Add(
-                            new KeyValuePair<int, KeyValuePair<Node, Node>>(
-                                child.Value,
-                                new KeyValuePair<Node, Node>(child.Key, graph[i, j])
-                            )
-                        );
-                    }
-                }
-            }
-            nodes.Sort((a, b) => a.Key.CompareTo(b.Key));
 
-            DisjointSet dsu = new DisjointSet(graph.GetLength(0) * graph.GetLength(1));
-            // Exact(M * log(M))
-            foreach (KeyValuePair<int, KeyValuePair<Node, Node>> child in nodes)
+        // Exact(M)
+        private static DisjointSet ImageSegmentation(
+            Dictionary<(int, int), List<Edge>> adjacencyList, List<Edge> sortedEdges, int height, int width)
+        {
+            int totalPixels = height * width;
+            DisjointSet dsu = new DisjointSet(totalPixels);
+
+            foreach (var edge in sortedEdges)
             {
-                dsu.Union(child.Value.Key.id, child.Value.Value.id, child.Key);
+                int fromId = edge.From.x * width + edge.From.y;
+                int toId = edge.To.x * width + edge.To.y;
+
+                dsu.Union(fromId, toId, edge.Weight);
             }
+
             return dsu;
         }
         private static RGBPixel GetColorForSegment(int id)
@@ -142,45 +165,50 @@ namespace ImageTemplate
         }
         public static void ImageProcess(ref RGBPixel[,] imageMatrix)
         {
+            // Get the height and width of the image matrix
+            int height = imageMatrix.GetLength(0);
+            int width = imageMatrix.GetLength(1);
+
             // Graphs for each color
-            Node[,] graphRed = GraphConstruct(imageMatrix, "red");
-            Node[,] graphGreen = GraphConstruct(imageMatrix, "green");
-            Node[,] graphBlue = GraphConstruct(imageMatrix, "blue");
+            var (graphRed, sortedRedEdges) = GraphConstruct(imageMatrix, "red");
+            var (graphGreen, sortedGreenEdges) = GraphConstruct(imageMatrix, "green");
+            var (graphBlue, sortedBlueEdges) = GraphConstruct(imageMatrix, "blue");
 
-            // Segment the image for each color
-            DisjointSet redSegments = ImageSegmentation(graphRed);
-            DisjointSet greenSegments = ImageSegmentation(graphGreen);
-            DisjointSet blueSegments = ImageSegmentation(graphBlue);
+            // Segment the image for each color using the adjacency list and sorted edges
+            DisjointSet redSegments = ImageSegmentation(graphRed, sortedRedEdges, height, width);
+            DisjointSet greenSegments = ImageSegmentation(graphGreen, sortedGreenEdges, height, width);
+            DisjointSet blueSegments = ImageSegmentation(graphBlue, sortedBlueEdges, height, width);
 
+            // Log the segmentation info (optional)
             LogSegmentInfo("Red", redSegments);
             LogSegmentInfo("Green", greenSegments);
             LogSegmentInfo("Blue", blueSegments);
 
-            // Intersect the 3 label maps
+            // Intersect the 3 label maps for the final segmentation
             var segmentColorsMap = new Dictionary<(int, int, int), RGBPixel>();
 
-            int height = imageMatrix.GetLength(0);
-            int width = imageMatrix.GetLength(1);
-
-
-            // Exact(N^2)
+            // Traverse the image to assign new colors based on the segment IDs
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
-                    int redId = redSegments.Find(graphRed[i, j].id);
-                    int greenId = greenSegments.Find(graphGreen[i, j].id);
-                    int blueId = blueSegments.Find(graphBlue[i, j].id);
+                    // Access the ID of the pixel in each color channel (flattened ID)
+                    int redId = redSegments.Find(i * width + j);  // Use flattened ID for red
+                    int greenId = greenSegments.Find(i * width + j);  // Use flattened ID for green
+                    int blueId = blueSegments.Find(i * width + j);  // Use flattened ID for blue
 
                     var combinedId = (redId, greenId, blueId);
 
+                    // If the combined segment does not exist, create a new color for it
                     if (!segmentColorsMap.ContainsKey(combinedId))
                         segmentColorsMap[combinedId] = GetColorForSegment(segmentColorsMap.Count);
 
+                    // Update the pixel with the color corresponding to its segment
                     imageMatrix[i, j] = segmentColorsMap[combinedId];
                 }
             }
         }
+
 
 
         // Worst Case: Exact(N^2)
